804a826218a8c4c7b34638956d9bc51d8d91751f
soroush83faraz <soroush83faraz@users.noreply.github.com>
2026-08-22 05:40:18 +0000

    preview-pr#43: سئو: کوتاه‌سازی متادسکریپشن سه صفحه (خانه، دسته‌ی ترموگرافی، مقاله UV-vs-IR) soroush83faraz/fmg-tech@00c04cb320fa234480b9501da6e4dd47680dbd61

---
+++ _next/static/SLPPf44m0fnYwS-R2nG1T/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[slug]","\u002Fcase-studies\u002F[slug]","\u002Fproduct\u002F[slug]","\u002Fproducts\u002F[categorySlug]","\u002Frepairs\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
